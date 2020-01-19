7d651b5f271154cd7e7526c2bac319323d64bb34
Luke Thompson <endeavour9@gmail.com>
2020-01-20 08:39:12 +1100

    temp checkin broken to switch devices

---
+++ java8/src/main/java/com/sydneyjavameetup/JsonSerialization.java
@@ -0,0 +1,9 @@
+package com.sydneyjavameetup;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+public class JsonSerialization {
+	public static final ObjectMapper objectMapper = new ObjectMapper();
+	public static final ObjectWriter prettyObjectWriter = objectMapper.writerWithDefaultPrettyPrinter();
+}
